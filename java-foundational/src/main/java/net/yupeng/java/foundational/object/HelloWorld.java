091b485273b450b5a6c09aee96c74143ddf8dea5
JamesYusling <13596273436@163.com>
2021-12-12 13:53:46 +0800

    新创建  module  java-foundational

---
+++ java-foundational/src/main/java/net/yupeng/java/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.yupeng.java.foundational.object;
+
+/**
+ * git 第一个 helloWorld
+ *
+ * @author JamesYusling/yupeng 13596273436@163.com
+ * @version 2021/12/12 13:49
+ * @since JDK17
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl windows10 & jdk17 &git &maven & idea2021.3");
+    }
+}
